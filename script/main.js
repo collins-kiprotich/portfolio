71e19c6255ca71e2d7ca2ac5f7089b880b07b0bf
collins kiprotich <collinskiprotich@gmail.com>
2022-11-14 20:38:16 +0300

    fix linter errors

--- script/main.js
+++ script/main.js
@@ -1,29 +1,29 @@
 const body = document.querySelector('body');
 const menuDiv = document.createElement('div');
-menuDiv.className="menu-content";
-menuDiv.style.display= 'none';
-let divContent = '<img src="./icon-images/Icon- cross icon.png" alt=""> <a href="#portfolio"> Portfolio</a> <a href="#aboutMe"> About</a> <a href="#form"> Contact</a>';
+menuDiv.className = 'menu-content';
+menuDiv.style.display = 'none';
+const divContent = '<img src="./icon-images/Icon- cross icon.png" alt=""> <a href="#portfolio"> Portfolio</a> <a href="#aboutMe"> About</a> <a href="#form"> Contact</a>';
 menuDiv.innerHTML = divContent;
 console.log(menuDiv);
 body.appendChild(menuDiv);
-let main = document.querySelector('main');
+const main = document.querySelector('main');
 
-let hamburgerMenu = document.querySelector('.right-side img');
-hamburgerMenu.addEventListener('click', ()=> {
-  main.style.filter= 'blur(0.3125rem)';
+const hamburgerMenu = document.querySelector('.right-side img');
+hamburgerMenu.addEventListener('click', () => {
+  main.style.filter = 'blur(0.3125rem)';
   menuDiv.style.display= 'flex';
 });
 
-let crossIcon=document.querySelectorAll(".menu-content img");
-crossIcon[0].addEventListener('click',()=> {
-    main.style.filter= 'blur(0)';
-    menuDiv.style.display= 'none';
+const crossIcon = document.querySelectorAll(".menu-content img");
+crossIcon[0].addEventListener('click', () => {
+  main.style.filter = 'blur(0)';
+  menuDiv.style.display = 'none';
 });
-let itemList = document.querySelectorAll('.menu-content a');
+const itemList = document.querySelectorAll('.menu-content a');
 
- itemList.forEach(item => {
-   item.addEventListener('click',()=> {
-    main.style.filter= 'blur(0)';
-    menuDiv.style.display= 'none';
+itemList.forEach((item) => {
+  item.addEventListener('click', () => {
+    main.style.filter = 'blur(0)';
+    menuDiv.style.display = 'none';
+  });
 });
- });
