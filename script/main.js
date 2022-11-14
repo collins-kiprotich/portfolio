50e25d05409b8a4ac5f1a67f9c930ab7dc990733
collins kiprotich <collinskiprotich@gmail.com>
2022-11-14 18:56:35 +0300

    fixed linter errors

--- script/main.js
+++ script/main.js
@@ -1,5 +1,5 @@
-let body = document.querySelector("body");
-let menuDiv=document.createElement("div");
+const body = document.querySelector('body');
+const menuDiv = document.createElement('div');
 menuDiv.className="menu-content";
 menuDiv.style.display= 'none';
 let divContent = '<img src="./icon-images/Icon- cross icon.png" alt=""> <a href="#portfolio"> Portfolio</a> <a href="#aboutMe"> About</a> <a href="#form"> Contact</a>';
