c059f945e0ed4fa6348656096c57231a463bbb0e
collins kiprotich <collinskiprotich@gmail.com>
2022-11-14 20:41:25 +0300

    fix linter erros

--- script/main.js
+++ script/main.js
@@ -4,17 +4,16 @@ menuDiv.className = 'menu-content';
 menuDiv.style.display = 'none';
 const divContent = '<img src="./icon-images/Icon- cross icon.png" alt=""> <a href="#portfolio"> Portfolio</a> <a href="#aboutMe"> About</a> <a href="#form"> Contact</a>';
 menuDiv.innerHTML = divContent;
-console.log(menuDiv);
 body.appendChild(menuDiv);
 const main = document.querySelector('main');
 
 const hamburgerMenu = document.querySelector('.right-side img');
 hamburgerMenu.addEventListener('click', () => {
   main.style.filter = 'blur(0.3125rem)';
-  menuDiv.style.display= 'flex';
+  menuDiv.style.display = 'flex';
 });
 
-const crossIcon = document.querySelectorAll(".menu-content img");
+const crossIcon = document.querySelectorAll('.menu-content img');
 crossIcon[0].addEventListener('click', () => {
   main.style.filter = 'blur(0)';
   menuDiv.style.display = 'none';
