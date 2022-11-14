6206ff1668516dfa755da66d2a6d70121f26904f
rbhatt199924 <rbhatt199924@gmail.com>
2022-11-14 19:54:26 +0530

    add js for hamburger menu

--- script/main.js
+++ script/main.js
@@ -1,7 +1,15 @@
 let body = document.querySelector("body");
 let menuDiv=document.createElement("div");
 menuDiv.className="menu-content";
-menuDiv.style.display= 'flex';
+menuDiv.style.display= 'none';
 let divContent = '<img src="./icon-images/Icon- cross icon.png" alt=""> <a href="#portfolio"> Portfolio</a> <a href="#aboutMe"> About</a> <a href="#form"> Contact</a>';
 menuDiv.innerHTML = divContent;
+console.log(menuDiv);
 body.appendChild(menuDiv);
+let main = document.querySelector('main');
+
+let hamburgerMenu = document.querySelector('.right-side img');
+hamburgerMenu.addEventListener('click', ()=> {
+  main.style.filter= 'blur(0.3125rem)';
+  menuDiv.style.display= 'flex';
+});
